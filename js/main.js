2f290f1435b4b948cb7e4b6875f0017dddb70a36
yushaezhik <ksushaezhikova@gmail.com>
2025-04-26 18:02:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,134 +1,61 @@
-/*  ---------------------------------------------------
-  Template Name: Activitar
-  Description:  Activitar Fitness HTML Template
-  Author: Colorlib
-  Author URI: https://colorlib.com
-  Version: 1.0
-  Created: Colorlib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
     $(window).on('load', function () {
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
 
-        /*------------------
-            Gallery filter
-        --------------------*/
         $('.gallery-controls ul li').on('click', function() {
-            $('.gallery-controls ul li').removeClass('active');
-            $(this).addClass('active');
+            $('.gallery-controls ul li').removeClass('active').end().addClass('active');
         });
-        if($('.gallery-filter').length > 0 ) {
-            var containerEl = document.querySelector('.gallery-filter');
-            var mixer = mixitup(containerEl);
+
+        if ($('.gallery-filter').length) {
+            mixitup(document.querySelector('.gallery-filter'));
         }
 
         $('.blog-gird').masonry({
-			itemSelector: '.grid-item',
-			columnWidth: '.grid-sizer',
-		});
-
+            itemSelector: '.grid-item', columnWidth: '.grid-sizer',
+        });
     });
 
-    /*------------------
-        Background Set
-    --------------------*/
     $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
+        $(this).css('background-image', 'url(' + $(this).data('setbg') + ')');
     });
 
-    /*------------------
-		Navigation
-	--------------------*/
     $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
+        prependTo: '#mobile-menu-wrap', allowParentLinks: true
     });
 
-    /*------------------
-		Menu Hover
-	--------------------*/
-    $(".header-section .nav-menu .mainmenu ul li").on('mousehover', function() {
+    $(".header-section .nav-menu .mainmenu ul li").on('mouseenter', function() {
         $(this).addClass('active');
-    });
-    $(".header-section .nav-menu .mainmenu ul li").on('mouseleave', function() {
+    }).on('mouseleave', function() {
         $('.header-section .nav-menu .mainmenu ul li').removeClass('active');
     });
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
     $(".hero-items").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
+        loop: true, margin: 0, nav: true, items: 1,
+        dots: true, animateOut: 'fadeOut', animateIn: 'fadeIn',
         navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
+        smartSpeed: 1200, autoHeight: false,
     });
 
-    /*------------------
-        Testimonial Slider
-    --------------------*/
-   $(".testimonial-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: false,
-        items: 1,
-        dots: true,
-        navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
+    $(".testimonial-slider").owlCarousel({
+        loop: true, margin: 0, nav: false, items: 1,
+        dots: true, navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
+        smartSpeed: 1200, autoHeight: false, autoplay: true,
     });
 
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
-
-    $('.image-popup').magnificPopup({
-        type: 'image'
-    });
-
-    /*------------------
-        Magnific Popup
-    --------------------*/
+    $('.video-popup').magnificPopup({ type: 'iframe' });
+    $('.image-popup').magnificPopup({ type: 'image' });
     $('.show-result-select').niceSelect();
 
-    /*------------------
-       Timetable Filter
-    --------------------*/
     $('.timetable-controls ul li').on('click', function() {
         var tsfilter = $(this).data('tsfilter');
-        $('.timetable-controls ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        if(tsfilter == 'all') {
-            $('.classtime-table').removeClass('filtering');
-            $('.ts-item').removeClass('show');
-        } else {
-            $('.classtime-table').addClass('filtering');
-        }
+        $('.timetable-controls ul li').removeClass('active').end().addClass('active');
+        $('.classtime-table').toggleClass('filtering', tsfilter !== 'all');
+
         $('.ts-item').each(function(){
-            $(this).removeClass('show');
-            if($(this).data('tsmeta') == tsfilter) {
-                $(this).addClass('show');
-            }
+            $(this).toggleClass('show', $(this).data('tsmeta') === tsfilter);
         });
     });
 
